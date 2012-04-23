740d4c9d0957a5364d910f5aaf59ff3054235fd7
Chun-hao Hu <hu.chunhao@gmail.com>
2012-04-24 00:00:59 +0100

    Added layout extension, monkey patched swig to support express 3

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -7,8 +6,11 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http');
 
+_ = require('underscore');
+
 var mongodb = require('mongodb');
-var cons = require('consolidate');
+//var cons = require('consolidate');
+var swig = require('swig');
 var app = express();
 
 var config = require('./config');
@@ -16,9 +18,28 @@ var host = config.host || 'localhost';
 var port = config.port || mongodb.Connection.DEFAULT_PORT;
 var db = new mongodb.Db(config.database, new mongodb.Server(host, port, {auto_reconnect: true}));
 
+swig.init({
+  root: __dirname + '/views',
+  allowErrors: false
+});
+
+/*
+* Monkey-patching swig until swig supports express
+*/
+swig.__express = function(path, options, fn) {
+  options = options || {};
+  try {
+    options.filename = path;
+    var tmpl = swig.compileFile(_.last(path.split('/')));
+    fn(null, tmpl.render(options));
+  } catch (err) {
+    fn(err);
+  }
+};
+
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.engine('.html', cons.swig);
+  app.engine('.html', swig.__express);
   app.set('view engine', 'html');
   app.set('view options', {layout: false});
   app.use(express.favicon());
