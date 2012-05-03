b953886329532b69a0f4aea41112962234e153f5
Chun-hao Hu <hu.chunhao@gmail.com>
2012-05-03 16:21:40 +0100

    Added test for utils file.

---
+++ test/utils.js
@@ -0,0 +1,13 @@
+var utils = require('../utils.js');
+var expect = require('chai').expect;
+
+describe('Utils', function() {
+  describe('parseCollectionName', function() {
+    it('should return the collection and database of a full collection namespace', function() {
+      var result = utils.parseCollectionName('databaseName.collection.x.y');
+
+      expect(result).to.have.property('name', 'collection.x.y').to.be.a('string');
+      expect(result).to.have.property('database', 'databaseName').to.be.a('string');
+    });
+  });
+});
