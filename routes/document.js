175fc9085a62050d80c707cdf9364a2f5bda8a6f
Chun-hao Hu <hu.chunhao@gmail.com>
2012-05-05 23:26:05 +0100

    Added CodeMirror for formatting documents.
    Replaced prettyprint on the collection view with CodeMirror.
    Added config option to specify CodeMirror theme.

--- routes/document.js
+++ routes/document.js
@@ -1,6 +1,9 @@
+var config = require('../config');
+
 exports.viewDocument = function(req, res, next) {
   var ctx = {
     title: 'Viewing Document: ' + req.document._id,
+    editorTheme: config.options.editorTheme
   };
 
   res.render('document', ctx);
