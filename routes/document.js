d0e77e9f4c2f66e9dfcda8020fe8164da5344e52
Chun-hao Hu <hu.chunhao@gmail.com>
2012-05-06 23:45:39 +0100

    Added reminder to change document updating function.

--- routes/document.js
+++ routes/document.js
@@ -10,6 +10,7 @@ exports.viewDocument = function(req, res, next) {
   res.render('document', ctx);
 };
 
+
 exports.updateDocument = function(req, res, next) {
   var doc = req.body.document;
 
@@ -30,6 +31,7 @@ exports.updateDocument = function(req, res, next) {
   var id = new mongodb.ObjectID.createFromHexString(docJSON._id);
   docJSON._id = id;
 
+  //TODO: change collection.save to collection.update, figure out how to use ObjectID
   req.collection.save(docJSON, {safe: true}, function(err, result) {
     if (err) {
       //TODO: handle error
