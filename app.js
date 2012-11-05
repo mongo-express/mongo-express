1e162e42a0b9ac4df99f5891bf2ec57e754a09c4
soomtong-onweekendpc <soomtong@gmail.com>
2012-11-05 13:39:06 +0900

    update res.locals

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ db.open(function(err, db) {
 });
 
 //View helper, sets local variables used in templates
-app.locals(function(req, res) {
+app.all('*', function(req, res, next) {
   res.locals.baseHref = config.site.baseUrl;
   res.locals.databases = databases;
   res.locals.collections = collections;
@@ -203,6 +203,8 @@ app.locals(function(req, res) {
     res.locals.messageError = req.session.error;
     delete req.session.error;
   }
+
+  next();
 });
 
 
