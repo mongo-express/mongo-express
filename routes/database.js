f79df11679ac7f93bacc4b1d88ba115b096fcd8a
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-01-26 22:41:01 -0500

    show stats in the db home pg

--- routes/database.js
+++ routes/database.js
@@ -1,7 +1,17 @@
+var utils = require('../utils');
+
 exports.viewDatabase = function(req, res) {
-  var ctx = {
-    title: 'Viewing Database: ' + req.dbName,
-    colls: req.collections[req.dbName]
-  };
-  res.render('database', ctx);
+  req.db.stats(function(err, data){
+	  var ctx = {
+		title: 'Viewing Database: ' + req.dbName,
+		colls: req.collections[req.dbName],
+		stats: {
+			collections: data.collections,
+			dataSize: utils.bytesToSize(data.dataSize),
+			storageSize: utils.bytesToSize(data.storageSize),
+			fileSize: utils.bytesToSize(data.fileSize)
+		}
+	  };
+	  res.render('database', ctx);
+  });  
 }
--- utils.js
+++ utils.js
@@ -9,3 +9,13 @@ exports.parseCollectionName = function parseCollectionName(full_name) {
   var database = coll_parts.splice(0,1);
   return { name: coll_parts.join('.'), database: database.toString() };
 };
+
+// Given some size in bytes, returns it in a converted, friendly size
+// credits: http://stackoverflow.com/users/1596799/aliceljm
+exports.bytesToSize = function bytesToSize(bytes) {
+   if(bytes == 0) return '0 Byte';
+   var k = 1000;
+   var sizes = [' bytes', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+   var i = Math.floor(Math.log(bytes) / Math.log(k));
+   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+}
