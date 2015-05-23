bcdd568b792465274d906884e5daa8ae0e4c6339
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2015-05-22 19:06:57 -0700

    In app.js, set mount path to '/' by default. Upgrade consolidate to fix cache error in production (https://github.com/tj/consolidate.js/pull/134).

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var
   ;
 
 var app = express();
-app.use('/db-admin', middleware(config));
+app.use('/', middleware(config));
 app.listen(config.site.port, function() {
   console.log("Mongo Express server listening on port " + (config.site.port || 80));
 });
