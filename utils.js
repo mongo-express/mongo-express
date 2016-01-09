7f6c844f2689298478fcd3bdc5a4902467babb76
Михаил <muxahuk1214@mail.ru>
2016-01-09 08:24:31 +0200

    Settings overwrite
    Settings can now overwrite default settings, no need to copy full
    settings file. Just change what u need.

--- utils.js
+++ utils.js
@@ -9,3 +9,43 @@ exports.bytesToSize = function bytesToSize(bytes) {
   var i = Math.floor(Math.log(bytes) / Math.log(k));
   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
 };
+
+exports.deepmerge = function(target, src) {
+  var array = Array.isArray(src);
+  var dst = array && [] || {};
+
+  if (array) {
+    target = target || [];
+    dst = dst.concat(target);
+    src.forEach(function(e, i) {
+      if (typeof dst[i] === 'undefined') {
+        dst[i] = e;
+      } else if (typeof e === 'object') {
+        dst[i] = exports.deepmerge(target[i], e);
+      } else {
+        if (target.indexOf(e) === -1) {
+          dst.push(e);
+        }
+      }
+    });
+  } else {
+    if (target && typeof target === 'object') {
+      Object.keys(target).forEach(function (key) {
+        dst[key] = target[key];
+      })
+    }
+    Object.keys(src).forEach(function (key) {
+      if (typeof src[key] !== 'object' || !src[key]) {
+        dst[key] = src[key];
+      } else {
+        if (!target[key]) {
+          dst[key] = src[key];
+        } else {
+          dst[key] = exports.deepmerge(target[key], src[key]);
+        }
+      }
+    });
+  }
+
+  return dst;
+}
