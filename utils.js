43dbdd598a7ce8b00ddf583dc8d37f44bfceada6
Wulf Solter <wulf@wulf.co.nz>
2016-01-20 00:22:46 +1300

    utils.bytesToSize - use proper SI units

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@
 exports.bytesToSize = function bytesToSize(bytes) {
   if (bytes === 0) return '0 Byte';
   var k = 1000;
-  var sizes = [' bytes', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb', 'zb', 'yb'];
+  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   var i = Math.floor(Math.log(bytes) / Math.log(k));
   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
 };
