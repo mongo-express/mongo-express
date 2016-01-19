4407322f128d5e8ce66052987283f3699ea77a95
Wulf Solter <wulf@wulf.co.nz>
2016-01-20 00:23:37 +1300

    utils.roughSizeOfObject - gives an approximation of the object size

--- utils.js
+++ utils.js
@@ -72,3 +72,30 @@ exports.deepmerge = function(target, src) {
 
   return dst;
 };
+
+exports.roughSizeOfObject = function(object) {
+
+  var objectList = [];
+  var recurse = function(value) {
+    var bytes = 0;
+
+    if (typeof value === 'boolean') {
+      bytes = 4;
+    } else if (typeof value === 'string') {
+      bytes = value.length * 2;
+    } else if (typeof value === 'number') {
+      bytes = 8;
+    } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
+      objectList[ objectList.length ] = value;
+
+      for (var i in value) {
+        bytes += 8; // an assumed existence overhead
+        bytes += recurse(value[i]);
+      }
+    }
+
+    return bytes;
+  };
+
+  return recurse(object);
+};
