5fed6a79c0a4b3ea017facec9f8fec82d5e96765
Hugo Dozois <hugo.dozois@gmail.com>
2020-10-24 21:36:24 -0400

    Change turn-on object-shorthand

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,7 +53,6 @@ module.exports = {
     'lodash/unwrap': ['error'],
 
     // To be turned on
-    'object-shorthand': ['off'],
     'no-path-concat': ['off'],
     'no-shadow': ['off', { allow: ['err', 'error'] }],
     camelcase: ['off'],
