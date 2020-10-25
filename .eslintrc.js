c749b29d056128efe7a3ac114eb50a0606445d6f
Hugo Dozois <hugo.dozois@gmail.com>
2020-10-24 21:39:08 -0400

    Change turn on no-path-concat

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,7 +53,6 @@ module.exports = {
     'lodash/unwrap': ['error'],
 
     // To be turned on
-    'no-path-concat': ['off'],
     'no-shadow': ['off', { allow: ['err', 'error'] }],
     camelcase: ['off'],
   },
