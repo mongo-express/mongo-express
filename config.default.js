7b34299af9087cd8cf30b21186fb980ef2525ae3
Zac Poorman <33203487+OOCAZ@users.noreply.github.com>
2022-09-21 09:33:59 -0600

    updated readme, config.default.js, and the gitignore to not track .env

--- config.default.js
+++ config.default.js
@@ -135,11 +135,11 @@ export default {
     // baseUrl: the URL that mongo express will be located at - Remember to add the forward slash at the start and end!
     baseUrl: process.env.ME_CONFIG_SITE_BASEURL || '/',
     cookieKeyName: 'mongo-express',
-    cookieSecret: process.env.ME_CONFIG_SITE_COOKIESECRET || 'cookiesecret',
+    cookieSecret: process.env.ME_CONFIG_SITE_COOKIESECRET,
     host: process.env.VCAP_APP_HOST || 'localhost',
     port: process.env.PORT || 8081,
     requestSizeLimit: process.env.ME_CONFIG_REQUEST_SIZE || '50mb',
-    sessionSecret: process.env.ME_CONFIG_SITE_SESSIONSECRET || 'sessionsecret',
+    sessionSecret: process.env.ME_CONFIG_SITE_SESSIONSECRET,
     sslCert: process.env.ME_CONFIG_SITE_SSL_CRT_PATH || '',
     sslEnabled: getBoolean(process.env.ME_CONFIG_SITE_SSL_ENABLED, false),
     sslKey: process.env.ME_CONFIG_SITE_SSL_KEY_PATH || '',
