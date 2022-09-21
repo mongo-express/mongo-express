dff551cba36471685c7d8875de6618a5ea74a59e
rtritto <r.tritto5@studenti.uniba.it>
2022-09-21 15:19:54 +0200

    Add unit tests of stringDocIDs

--- test/lib/filtersSpec.js
+++ test/lib/filtersSpec.js
@@ -1,8 +1,30 @@
+import { Binary, ObjectId } from 'bson';
 import { expect } from 'chai';
+import { v4 as uuidv4 } from 'uuid';
 // eslint-disable-next-line camelcase
-import { to_display } from '../../lib/filters.js';
+import { stringDocIDs, to_display } from '../../lib/filters.js';
 
 describe('filters', function () {
+  describe('stringDocIDs', function () {
+    describe('should test a Binary BSON type', () => {
+      it('not v4');
+      it('v4', () => {
+        const UUID = uuidv4();
+        const hex = UUID.split('-').join('');
+        const buffer = new Buffer.from(hex, 'hex');
+        const input = new Binary(buffer, Binary.SUBTYPE_UUID);
+        expect(stringDocIDs(input)).to.equal(`UUID("${UUID}")`);
+      });
+    });
+    it('should test an ObjectID BSON type', () => {
+      const input = new ObjectId();
+      expect(stringDocIDs(input)).to.equal(input.toString());
+    });
+    it('should test a not BSON type', () => {
+      const input = {};
+      expect(stringDocIDs(input)).to.equal(input);
+    });
+  });
   describe('to_display', function () {
     it('should escape properly a string', () => {
       const result = to_display('<script>window.alert(\'alert 1!\')</script>');
