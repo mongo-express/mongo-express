438a4c721fdfcb6bb060562a3b670b77675e83a0
rtritto <r.tritto5@studenti.uniba.it>
2022-09-22 10:44:26 +0200

    Fix parse of Binary ID for document detail

--- test/lib/routers/documentSpec.js
+++ test/lib/routers/documentSpec.js
@@ -1,8 +1,10 @@
+import { Binary } from 'bson';
 import { expect } from 'chai';
+import { v4 as uuidv4 } from 'uuid';
 
 import { createServer, getDocumentUrl } from '../../testHttpUtils.js';
 import {
-  cleanAndCloseDb, initializeDb, getFirstDocumentId, testDbName as dbName, testURLCollectionName,
+  cleanAndCloseDb, initializeDb, getFirstDocumentId, testDbName as dbName, testCollection, testURLCollectionName,
 } from '../../testMongoUtils.js';
 
 // const collectionName = mongoUtils.testCollectionName;
@@ -21,12 +23,25 @@ describe('Router document', () => {
       close = server.close;
     }));
 
-  it('GET /db/<dbName>/<collection>/<document> should return html', () => {
+  it('GET /db/<dbName>/<collection>/<document> (_id: ObjectId) should return html', () => {
     const docId = getFirstDocumentId();
     return request.get(getDocumentUrl(dbName, urlColName, docId)).expect(200)
       .then((res) => {
         expect(res.text).to.match(new RegExp(`<title>${docId} - Mongo Express</title>`));
       });
+  });
+  it('GET /db/<dbName>/<collection>/<document> (_id: UUID) should return html', async () => {
+    const UUID = uuidv4();
+    const hex = UUID.split('-').join('');
+    const buffer = new Buffer.from(hex, 'hex');
+    const _id = new Binary(buffer, Binary.SUBTYPE_UUID);
+    const doc = { _id };
+    await testCollection(db).insertOne(doc);
+    return request.get(getDocumentUrl(dbName, urlColName, UUID)).query({ subtype: Binary.SUBTYPE_UUID }).expect(200)
+      .then((res) => {
+        expect(res.text).to.match(new RegExp(`<title>${UUID} - Mongo Express</title>`));
+      })
+      .finally(() => testCollection(db).deleteOne({ _id }));
   });
 
   it('POST /db/<dbName>/<collection> should add a new document');
