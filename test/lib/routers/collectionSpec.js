7e6d91ba380c898b71dfd75d761d6674341d6213
rtritto <40242971+rtritto@users.noreply.github.com>
2022-09-28 19:51:31 +0200

    Fix export functions (#993)
    * Restore query for non-aggregate cases
    
    * Add console error to collection
    
    * Add unit tests to endpoints of file export and json extension
    
    * Add unit tests for aggregate query

--- test/lib/routers/collectionSpec.js
+++ test/lib/routers/collectionSpec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import htmlParser from 'node-html-parser';
 
 import { createServer } from '../../testHttpUtils.js';
 import {
@@ -6,6 +7,7 @@ import {
 } from '../../testMongoUtils.js';
 
 describe('Router collection', () => {
+  /** @type {import('supertest').SuperAgentTest} */
   let request;
   let close;
   let client;
@@ -18,22 +20,79 @@ describe('Router collection', () => {
       close = server.close;
     }));
 
-  it('GET /db/<dbName>/<collection> should return html', () => request.get(`/db/${dbName}/${urlColName}`).expect(200)
-    .then((res) => {
-      expect(res.text).to.match(new RegExp(`<title>${collectionName} - Mongo Express</title>`));
-      expect(res.text).to.match(new RegExp(`<h1 id="pageTitle">Viewing Collection: ${collectionName}</h1>`));
-    }));
+  describe('GET /db/<dbName>/<collection> should return html', () => {
+    it('No query - _getQuery.result=nul', () => request
+      .get(`/db/${dbName}/${urlColName}`).expect(200)
+      .then((res) => {
+        expect(res.text).to.match(new RegExp(`<title>${collectionName} - Mongo Express</title>`));
+        expect(res.text).to.match(new RegExp(`<h1 id="pageTitle">Viewing Collection: ${collectionName}</h1>`));
+        expect(htmlParser.parse(res.text).querySelectorAll('[id^="doc-"]').length).to.equal(4);
+      }));
+
+    it('query={testItem:1} - _getQuery.result={testItem: 1}', () => request
+      .get(`/db/${dbName}/${urlColName}`).expect(200).query({ query: '{testItem:1}' })
+      .then((res) => {
+        expect(res.text).to.match(new RegExp(`<title>${collectionName} - Mongo Express</title>`));
+        expect(res.text).to.match(new RegExp(`<h1 id="pageTitle">Viewing Collection: ${collectionName}</h1>`));
+        expect(htmlParser.parse(res.text).querySelectorAll('[id^="doc-"]').length).to.equal(1);
+      }));
+
+    describe('runAggregate=on', () => {
+      it('query= - _getQuery.result=null', () => request
+        .get(`/db/${dbName}/${urlColName}`).expect(200).query({ runAggregate: 'on', query: '' })
+        .then((res) => {
+          expect(res.text).to.match(new RegExp(`<title>${collectionName} - Mongo Express</title>`));
+          expect(res.text).to.match(new RegExp(`<h1 id="pageTitle">Viewing Collection: ${collectionName}</h1>`));
+          expect(htmlParser.parse(res.text).querySelectorAll('[id^="doc-"]').length).to.equal(4);
+        }));
+
+      it('query=[] - _getQuery.result=[]', () => request
+        .get(`/db/${dbName}/${urlColName}`).expect(200).query({ runAggregate: 'on', query: '[]' })
+        .then((res) => {
+          expect(res.text).to.match(new RegExp(`<title>${collectionName} - Mongo Express</title>`));
+          expect(res.text).to.match(new RegExp(`<h1 id="pageTitle">Viewing Collection: ${collectionName}</h1>`));
+          expect(htmlParser.parse(res.text).querySelectorAll('[id^="doc-"]').length).to.equal(4);
+        }));
+
+      it('query=[{$match:{testItem:1}}] - _getQuery.result=[{$match:{testItem:1}}]', () => request
+        .get(`/db/${dbName}/${urlColName}`).expect(200).query({ runAggregate: 'on', query: '[{$match:{testItem:1}}]' })
+        .then((res) => {
+          expect(res.text).to.match(new RegExp(`<title>${collectionName} - Mongo Express</title>`));
+          expect(res.text).to.match(new RegExp(`<h1 id="pageTitle">Viewing Collection: ${collectionName}</h1>`));
+          expect(htmlParser.parse(res.text).querySelectorAll('[id^="doc-"]').length).to.equal(1);
+        }));
+    });
+  });
 
   it('POST /db/<dbName> should add a new collection');
   it('DEL /db/<dbName>/<collection> should delete the collection');
   it('PUT /db/<dbName>/<collection> should rename the collection');
 
   it('GET /db/<dbName>/compact/<collection> should compact');
-  it('GET /db/<dbName>/expArr/<collection> should export as array');
-  it('GET /db/<dbName>/expCsv/<collection> should export as csv');
   it('GET /db/<dbName>/reIndex/<collection> should reIndex');
   it('PUT /db/<dbName>/addIndex/<collection> should addIndex');
-  it('GET /db/<dbName>/export/<collection> should export as json');
+
+  it('GET /db/<dbName>/export/<collection> should export as json', () => request
+    .get(`/db/${dbName}/export/${urlColName}`).expect(200).responseType('blob')
+    .then((res) => {
+      expect(res.headers['content-disposition']).to.equal(`attachment; filename="${collectionName}.json"; filename*=UTF-8''${collectionName}.json`);
+      expect(res.headers['content-type']).to.equal('application/json');
+    }));
+
+  it('GET /db/<dbName>/expArr/<collection> should export as array', () => request
+    .get(`/db/${dbName}/expArr/${urlColName}`).expect(200).responseType('blob')
+    .then((res) => {
+      expect(res.headers['content-disposition']).to.equal(`attachment; filename="${collectionName}.json"; filename*=UTF-8''${collectionName}.json`);
+      expect(res.headers['content-type']).to.equal('application/json');
+    }));
+
+  it('GET /db/<dbName>/expCsv/<collection> should export as csv', () => request
+    .get(`/db/${dbName}/expCsv/${urlColName}`).expect(200).responseType('blob')
+    .then((res) => {
+      expect(res.headers['content-disposition']).to.equal(`attachment; filename="${collectionName}.csv"; filename*=UTF-8''${collectionName}.csv`);
+      expect(res.headers['content-type']).to.equal('text/csv');
+    }));
+
   it('GET /db/<dbName>/dropIndex/<collection> should drop index');
   it('GET /db/<dbName>/updateCollections/<collection> should updateCollections');
 
